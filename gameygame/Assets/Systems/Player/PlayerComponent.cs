6189ed2440ace119f20fb9398507fd5e29f15fda
Emil Wasilewski <me@limered.de>
2018-12-01 21:31:09 +0100

    implemented player movement

--- gameygame/Assets/Systems/Player/PlayerComponent.cs
+++ gameygame/Assets/Systems/Player/PlayerComponent.cs
@@ -1,8 +1,13 @@
 ﻿using SystemBase;
+using Systems.GameState.Physics;
+using UnityEngine;
 
 namespace Systems.Player
 {
+    [RequireComponent(typeof(OldschoolPhysicComponent))]
     public class PlayerComponent : GameComponent
     {
+        public float JumpTakeofSpeed = 7;
+        public float MovementMaxSpeed = 7;
     }
 }
