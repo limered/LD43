6a7db4c0da60f5ab2d495a93a972ba9f743f9167
Emil Wasilewski <me@limered.de>
2018-12-02 15:33:55 +0100

    change combat to be event based

--- gameygame/Assets/Systems/Combat/Events/CombatEvtProjectileHit.cs
+++ gameygame/Assets/Systems/Combat/Events/CombatEvtProjectileHit.cs
@@ -2,7 +2,7 @@
 
 namespace Systems.Combat.Events
 {
-    public class ProjectileMsgHit
+    public class CombatEvtProjectileHit
     {
         public ProjectileComponent Projectile;
         public RaycastHit2D HitData;
