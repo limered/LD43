7ec55b8e4cdc843530fd62f06f95e796da7adfaf
Emil Wasilewski <me@limered.de>
2018-12-03 11:28:21 +0100

    add trigger system and game stats

---
+++ gameygame/Assets/Systems/ActionTrigger/ActionTriggerSystem.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using SystemBase;
+
+namespace Systems.ActionTrigger
+{
+    [GameSystem]
+    public class ActionTriggerSystem : GameSystem<TriggerComponent>
+    {
+        public override void Register(TriggerComponent component)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class TriggerComponent : GameComponent
+    {
+        public Action ActionToTrigger;
+    }
+}
---
+++ gameygame/Assets/Systems/GameStatistics/GameProgressSystem.cs
@@ -0,0 +1,18 @@
+﻿using SystemBase;
+
+namespace Systems.GameStatistics
+{
+    [GameSystem]
+    public class GameProgressSystem : GameSystem<CollectibleComponent>
+    {
+        public override void Register(CollectibleComponent component)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+
+    public class CollectibleComponent : GameComponent
+    {
+        public string UniqueId;
+    }
+}
